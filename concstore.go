1824839cf1420f49fd2097a0a5b505285b7cdc1e
E. H. Rast <erich@snafu.de>
2020-05-15 18:01:51 +0100

    Removed New.

--- concstore.go
+++ concstore.go
@@ -5,16 +5,11 @@ import (
 )
 
 // Storage is a concurrent key-value in-memory storage. Keys are uin64 and returned when an object is added.
+// You create a new storage by using &Storage{}.
 type Storage struct {
 	c     uint64
 	data  sync.Map
 	mutex sync.Mutex
-}
-
-// New returns a new storage with initial size.
-func New() *Storage {
-	s := Storage{}
-	return &s
 }
 
 // Add obj to the storage, return the object's key as uint64.
@@ -38,6 +33,8 @@ func (s *Storage) Delete(key uint64) {
 
 // Free the memory for the storage.
 func (s *Storage) Free() {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 	s.data.Range(func(k, v interface{}) bool {
 		s.data.Delete(k)
 		return true
